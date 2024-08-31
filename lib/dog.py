e4b58c0b8de3cede975cb3110940f68816f7d5fc
Vitalis Ian <viterlis254@gmail.com>
2024-08-31 09:54:27 +0300

    initial

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,15 @@
 
 class Dog:
     pass
+
+    """Class representing a dog with a name and breed."""
+
+    def __init__(self, name, breed="Mutt"):
+        """
+        Initialize a new Dog instance.
+
+        :param name: Name of the dog
+        :param breed: Breed of the dog, defaults to "Mutt" if not provided
+        """
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,12 @@
 
 class Person:
     pass
+    """Class representing a person with a name."""
+
+    def __init__(self, name):
+        """
+        Initialize a new Person instance.
+
+        :param name: Name of the person
+        """
+        self.name = name
